fc3e30e225957ec3a76354101201c0386573a4ff
sergio.marino <sergio.marino@beeva.com>
2018-05-29 11:02:41 +0200

    creada url /project y /Schedule.xlsx para bajarse ese fichero

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from pymongo import MongoClient
 import hashlib
+import os.path
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+
 
 client = MongoClient('localhost', 27017)
 db = client.pitufos
@@ -12,23 +14,12 @@ db = client.pitufos
 def get_questions():
     recommendation = db.recommendation
     output = []
+
     for item in recommendation.find():
-        output.append({'question': item['question'], 'answers': item['answers'], 'type': item['type']})
+        output.append({'question': item['question'], 'answers': item['answers'], 'type': item['type'], 'order': item['order']})
+
 
     return jsonify({'result': output})
-
-
-
-@app.route('/deleteRecommendation', methods=['GET'])
-def delete_questions():
-    recommendation = db.recommendation
-    recommendation.remove()
-    output = []
-    for item in recommendation.find():
-        output.append({'question': item['question'], 'answers': item['answers'], 'type': item['type']})
-
-    return jsonify({'result': output})
-
 
 @app.route('/recommendation', methods=['POST'])
 def save_questions():
@@ -36,12 +27,24 @@ def save_questions():
     question = request.json['question']
     answers = request.json['answers']
     type = request.json['type']
+    order = request.json['order']
     output = []
 
-    question_id = recommendation.insert({'question': question, 'answers': answers, 'type': type})
+    question_id = recommendation.insert({'question': question, 'answers': answers, 'type': type , 'order': order})
     question = recommendation.find_one({'_id': question_id})
 
-    output.append({'question': question['question'], 'answers': question['answers'], 'type': question['type']})
+    output.append({'question': question['question'], 'answers': question['answers'], 'type': question['type'], 'order': question['order']})
+    return jsonify({'result': output})
+
+
+@app.route('/recommendation', methods=['DELETE'])
+def delete_questions():
+    recommendation = db.recommendation
+    recommendation.remove()
+    output = []
+    for item in recommendation.find():
+        output.append({'question': item['question'], 'answers': item['answers'], 'type': item['type'], 'order' : item['order']})
+
     return jsonify({'result': output})
 
 @app.route('/users', methods=['GET'])
@@ -50,12 +53,11 @@ def get_users():
     output = []
     for user in users.find():
         output.append({'email': user['email'], 'passwd': user['passwd'], 'name': user['name'],
-                       'lastname': user['lastname'], 'photo': user['photo'], 'description': user['description'],
+                       'lastname': user['lastname'], 'img': user['img'], 'description': user['description'],
                        'sendEmail': user['sendEmail']})
 
-
-
     return jsonify({'result': output})
+
 
 @app.route('/users', methods=['POST'])
 def save_users():
@@ -67,19 +69,72 @@ def save_users():
     name = request.json['name']
     lastname = request.json['lastname']
     passwd = name + pre_pass2 + lastname
-    photo = request.json['photo']
+    img = request.json['img']
     description = request.json['description']
     sendEmail = request.json['sendEmail']
 
     user_id = users.insert({'email': email, 'passwd': passwd, 'name': name,
-                       'lastname': lastname, 'photo': photo, 'description': description,
+                       'lastname': lastname, 'img': img, 'description': description,
                        'sendEmail': sendEmail})
     user = users.find_one({'_id': user_id})
 
     output.append({'email': user['email'], 'passwd': user['passwd'], 'name': user['name'],
-                       'lastname': user['lastname'], 'photo': user['photo'], 'description': user['description'],
+                       'lastname': user['lastname'], 'img': user['img'], 'description': user['description'],
                        'sendEmail': user['sendEmail']})
     return jsonify({'result': output})
+
+
+@app.route('/users', methods=['DELETE'])
+def delete_users():
+    users = db.users
+    users.remove()
+    output = []
+    for user in users.find():
+        output.append({'email': user['email'], 'passwd': user['passwd'], 'name': user['name'],
+                       'lastname': user['lastname'], 'img': user['img'], 'description': user['description'],
+                       'sendEmail': user['sendEmail']})
+
+    return jsonify({'result': output})
+
+
+
+
+@app.route('/projects', methods=['GET'])
+def get_projects():
+    projects = db.projects
+    output = []
+    for project in projects.find():
+        output.append({'technologies': project['technologies'], 'workday': project['workday'], 'schedule': project['schedule'],
+                       'location': project['location']})
+
+    return jsonify({'result': output})
+
+@app.route('/projects', methods=['POST'])
+def save_projects():
+    projects = db.projects
+    output = []
+    technologies = request.json['technologies']
+    workday = request.json['workday']
+    schedule = request.json['schedule']
+    location = request.json['location']
+
+    project_id = projects.insert({'technologies': technologies, 'workday': workday, 'schedule': schedule,
+                            'location': location})
+
+    project = projects.find_one({'_id': project_id})
+
+    output.append({'technologies': project['technologies'], 'workday': project['workday'], 'schedule': project['schedule'],
+                   'location': project['location']})
+    return jsonify({'result': output})
+
+
+
+
+
+
+
+
+
 
 
 
